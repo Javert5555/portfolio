cf7a32757d7c3bb5d4d0d74ddc3e7f3aecd71c21
Maxwell Rutherford <javert.hugo@yandex.ru>
2023-07-15 19:48:36 +0300

    add about section

--- js/app.js
+++ js/app.js
@@ -1,5 +1,28 @@
+window.addEventListener('load', () => { /* Страница загружена, включая все ресурсы */
+  const preloader = document.querySelector('.preloader') /* находим блок Preloader */
+  setTimeout(() => {
+    preloader.classList.add('preloader_hidden') /* добавляем ему класс для скрытия */
+  }, 500)
+})
+
 const scrolling = window.addEventListener('scroll', e => {
-    document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
+  document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
+  
+  // const element = document.querySelector('.main-header')
+  // const rect = element.getBoundingClientRect()
+  // const viewportHeight = window.innerHeight
+
+  // const visibleHeight = Math.min(viewportHeight, rect.bottom) - Math.max(0, rect.top)
+
+
+  // const visiblePercent = visibleHeight / rect.height
+
+  // if (visiblePercent > 0.6) visiblePercent = 1
+
+  // document.body.style.cssText += `--mainHeaderVisiblePercent: ${visiblePercent}`
+
+
+  // console.log('Visible percent:', visiblePercent)
 })
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
@@ -7,6 +30,17 @@ ScrollSmoother.create({
     wrapper: '.wrapper',
     content: '.content'
 })
+
+// gsap.from(['.layers__header', '.layers__btn'], {
+//     y: 300,
+//     opacity: 0,
+//     duration: 2,
+//     // stagger: 0.1,
+//     // ease: "back.out(1.7)",
+// })
+
+// gsap.fromTo(".line", { width: 300, duration: 1 });
+// gsap.fromTo(".title", { scaleX: 0 }, { scaleX: 1, duration: 1, transformOrigin: "left" });
 
 const mySwiper = new Swiper('.slider', {
     // direction: 'vertical', // вертикальная прокрутка
@@ -18,12 +52,53 @@ const mySwiper = new Swiper('.slider', {
     loop:true,
     spaceBetween: 28,
     parallax: true,
+    // mousewheel: true,
 
 })
 
-window.addEventListener('load', () => { /* Страница загружена, включая все ресурсы */
-  const preloader = document.querySelector('.preloader') /* находим блок Preloader */
-  setTimeout(() => {
-    preloader.classList.add('preloader_hidden') /* добавляем ему класс для скрытия */
-  }, 500)
-})
+
+document.querySelector(".layers__btn").addEventListener("click", function() {
+    window.scrollTo({
+        top: window.innerHeight + 1,
+        behavior: 'smooth'
+    });
+});
+
+
+const lines = Array.from(document.querySelectorAll('.line'))
+const titles = Array.from(document.querySelectorAll('.title'))
+const sliderHeader = Array.from(document.querySelectorAll('.slider__header'))
+const sliderDescription = Array.from(document.querySelectorAll('.slider__description'))
+const sliderBtnCont = Array.from(document.querySelectorAll('.slider__btn-cont'))
+const sliderItem = Array.from(document.querySelectorAll('.slider__item'))
+
+const gsapAnimateTo = (elements, params) => {
+  elements.map(el => {
+    const observer = new IntersectionObserver(entries => {
+        if (entries[0].isIntersecting) {
+          gsap.to(el, params)
+          observer.disconnect()
+        }
+      });
+      observer.observe(el)
+  })
+}
+
+const gsapAnimateFrom = (elements, params) => {
+  elements.map(el => {
+    const observer = new IntersectionObserver(entries => {
+        if (entries[0].isIntersecting) {
+          gsap.from(el, params)
+          observer.disconnect()
+        }
+      });
+      observer.observe(el)
+  })
+}
+
+gsapAnimateTo(lines, { width: 250, duration: 1.5, opacity: 1 })
+gsapAnimateTo(titles, { duration: 2.5, opacity: 1 })
+gsapAnimateTo(sliderHeader, { duration: 1.5, opacity: 1 })
+gsapAnimateTo(sliderDescription, { duration: 1.5, opacity: 1, delay: 0.15 })
+gsapAnimateTo(sliderBtnCont, { duration: 1.5, opacity: 1, delay: 0.3 })
+
